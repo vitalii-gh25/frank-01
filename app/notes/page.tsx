332142d574515d3662ab6715e6d427d2b287621b
Vitalii_gh <a.g.vitalii2022@gmail.com>
2025-12-20 22:02:54 +0200

    delete extra files

--- app/notes/page.tsx
+++
@@ -1,8 +0,0 @@
-// app/notes/page.tsx
-
-import { redirect } from 'next/navigation';
-
-export default function NotesPage() {
-  // Перенаправлення на "all notes"
-  redirect('/notes/filter/all');
-}
